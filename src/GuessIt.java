93f28735f205a0b2e786729c8861497cecdff7fb
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-28 11:53:55 -0500

    feat: switch statements

--- src/GuessIt.java
+++ src/GuessIt.java
@@ -6,12 +6,30 @@ public class GuessIt {
 
         System.out.printf("Generated number is: %d.%n",randomNumber);
 
-        if (randomNumber == 1) {
-            System.out.println("red");
-        } else if (randomNumber == 2) {
-            System.out.println("green");
-        } else {
-            System.out.println("blue");
+//        if (randomNumber == 1) {
+//            System.out.println("red");
+//        } else if (randomNumber == 2) {
+//            System.out.println("green");
+//        } else {
+//            System.out.println("blue");
+//        }
+
+//        switch (randomNumber) {
+//            case 1:
+//                System.out.println("red");
+//                break;
+//            case 2:
+//                System.out.println("green");
+//                break;
+//            default:
+//                System.out.println("blue");
+//        }
+
+        // enhanced switch
+        switch (randomNumber) {
+            case 1 -> System.out.println("red");
+            case 2 -> System.out.println("green");
+            default -> System.out.println("blue");
         }
     }
 }
