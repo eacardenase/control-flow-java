d1e3fe4517a279f7e7b17c5b79ddd35e2a28d1f7
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-28 11:27:54 -0500

    feat: working with conditionals

--- src/GuessIt.java
+++ src/GuessIt.java
@@ -1,2 +1,17 @@
+import java.util.Random;
+
 public class GuessIt {
+    public static void main(String[] args) {
+        int randomNumber = new Random().nextInt(3) + 1;
+
+        System.out.printf("Generated number is: %d.%n",randomNumber);
+
+        if (randomNumber == 1) {
+            System.out.println("red");
+        } else if (randomNumber == 2) {
+            System.out.println("green");
+        } else {
+            System.out.println("blue");
+        }
+    }
 }
