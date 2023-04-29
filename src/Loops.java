4436a2aed9cf64cdd51bbe5e5fa5207bf60d644f
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-29 09:13:04 -0500

    feat: quit the program and regex validation

--- src/Loops.java
+++ src/Loops.java
@@ -3,18 +3,22 @@ import java.util.Random;
 public class Loops {
     public static void main(String[] args) {
         int randomNum = new Random().nextInt(10) + 1;
+        String guessedNumStr = null;
 
-        while (true) {
-            String guessedNumStr = System.console()
+        while (!"q".equals(guessedNumStr)) {
+            guessedNumStr = System.console()
                     .readLine("Please guess a number between 1 and 10 inclusively: ");
-            int guessedNum = Integer.parseInt(guessedNumStr);
 
-            if (randomNum == guessedNum) {
-                System.out.printf("The random number was %d. You got it!%n", randomNum);
+            if (guessedNumStr.matches("-?\\d{1,2}")) {
+                int guessedNum = Integer.parseInt(guessedNumStr);
 
-                break;
-            } else {
-                System.out.println("You didn't get it!");
+                if (randomNum == guessedNum) {
+                    System.out.printf("The random number was %d. You got it!%n", randomNum);
+
+                    break;
+                } else {
+                    System.out.println("You didn't get it!");
+                }
             }
         }
     }
